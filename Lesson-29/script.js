c612c4b75002a756dadb1c36be58f412d26b8dc3
KookiDi <pivtorak.roma@gmail.com>
2023-02-28 15:33:46 +0200

    Have done the twenty-ninth lesson

---
+++ Lesson-29/script.js
@@ -0,0 +1,11 @@
+let name = prompt("Як вас завати?","");
+    alert(`Привіт ${name}`);
+
+let age = prompt("Вкажіть ваш рік народження", "");
+const currentYear = 2023;
+let currentAge = currentYear - age;
+    alert(`Вам ${currentAge} років`);
+
+let squareLength = prompt("Вкажіть сторону квадрата","");
+let perimeter = (squareLength * 4);
+    alert(`Периметер квадрата: ${perimeter}`)
